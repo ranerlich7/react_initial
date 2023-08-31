29abfbfe9aa57bccd193a56e1757128f171753d6
ran <ran@goz.com>
2023-08-31 20:03:06 +0300

    added number as input to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 function Button() {
   const [counter, setCounter] = useState(1);
+  const [number1, setNumber1] = useState(1);
   return (
     <>
-      <button className="btn btn-info" onClick={() => setCounter(counter + 1)}>
+      <input value={number1} onChange={(e) => setNumber1(Number(e.target.value))} />
+      <button className="btn btn-info" onClick={() => setCounter(counter + number1)}>
         Counter +
       </button>
       <h1>{counter}</h1>
