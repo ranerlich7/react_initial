3150ecf09e17104b868d706fceb4b1838a293b8e
ran <ranerlich@gmail.com>
2023-09-01 12:33:25 +0300

    starting to add calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,6 +6,14 @@ function Button() {
   const [counter, setCounter] = useState(0);
   return (
     <>
+      {/* 
+      1. add 2 states for input fields
+      2. when i change the value in an input field- how is the state changed? onChange
+      3. take value from 2 fields add them and update the counter
+      */}
+      Num1: <input value="1" />
+      <br />
+      Num2: <input></input>
       <button className="btn btn-info" onClick={() => setCounter(counter + 1)}>
         Press +
       </button>
