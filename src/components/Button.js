b194c8488e7899e570ca33406543707682a462f0
ran <ranerlich@gmail.com>
2023-09-01 12:22:04 +0300

    added + and - button with use state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,17 @@
 import React, { useState } from "react";
 
-// // data = state
-// let index = 0;
-//   console.log(index);
-//   index += 1;
-//   document.getElementById("mytext").innerHTML = index;
-
 function Button() {
-  function increase() {
-    setCounter(counter + 1);
-  }
   // this is the way to define state in react
   // explanation -> const ["statename", "setStatename _function"] = useState("defaultvalue")
   const [counter, setCounter] = useState(0);
   return (
     <>
-      <button className="btn btn-info" onClick={increase}>
-        Press this button{" "}
+      <button className="btn btn-info" onClick={() => setCounter(counter + 1)}>
+        Press +
+      </button>
+      &nbsp;
+      <button className="btn btn-info" onClick={() => setCounter(counter - 1)}>
+        Press -
       </button>
       <div>{counter}</div>
     </>
