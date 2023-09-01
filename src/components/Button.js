5887eca50b48800b4d3daa9c151e17caa7ea1021
ran <ranerlich@gmail.com>
2023-09-01 13:07:58 +0300

    added calculator functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,8 @@ function Button() {
   // this is the way to define state in react
   // explanation -> const ["statename", "setStatename _function"] = useState("defaultvalue")
   const [counter, setCounter] = useState(0);
+  const [num1, setNum1] = useState("0");
+  const [num2, setNum2] = useState("0");
   return (
     <>
       {/* 
@@ -11,14 +13,22 @@ function Button() {
       2. when i change the value in an input field- how is the state changed? onChange
       3. take value from 2 fields add them and update the counter
       */}
-      Num1: <input value="1" />
+      Num1: <input value={num1} onChange={(e) => setNum1(e.target.value)} />
       <br />
-      Num2: <input></input>
-      <button className="btn btn-info" onClick={() => setCounter(counter + 1)}>
+      Num2: <input value={num2} onChange={(e) => setNum2(e.target.value)} />
+      {/* Plus action */}
+      <button
+        className="btn btn-info"
+        onClick={() => setCounter(parseFloat(num1) + parseFloat(num2))}
+      >
         Press +
       </button>
       &nbsp;
-      <button className="btn btn-info" onClick={() => setCounter(counter - 1)}>
+      {/* minus action */}
+      <button
+        className="btn btn-info"
+        onClick={() => setCounter(parseFloat(num1) - parseFloat(num2))}
+      >
         Press -
       </button>
       <div>{counter}</div>
